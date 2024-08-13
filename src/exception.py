bdf8e35d0da6b5d7183edbbaf60d4eb2003554f5
Ramesh Suragam <ramesh.s.data@gmail.com>
2024-08-13 14:42:23 +0100

    testing logger & exception

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -12,8 +13,17 @@ def error_message_detail(error, error_detail: sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
+    
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    
+    except Exception as e:
+        logging.info("Logging has started")
+        raise CustomException(e,sys)
