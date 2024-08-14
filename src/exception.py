ca1b5695575ca17d64061570b583347e573284ff
Ramesh Suragam <ramesh.s.data@gmail.com>
2024-08-14 16:18:24 +0100

    Data Ingestion and data

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -25,5 +25,5 @@ if __name__=="__main__":
         a=1/0
     
     except Exception as e:
-        logging.info("Logging has started")
+        logging.info("Divide by Zero")
         raise CustomException(e,sys)
