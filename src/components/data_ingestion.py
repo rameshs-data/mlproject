42ed179b0b6f29ba952a71fbb0b848f0262d6729
Ramesh Suragam <ramesh.s.data@gmail.com>
2024-08-15 22:54:11 +0100

    adding data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,12 +2,16 @@ from email import header
 from operator import index
 import os
 import sys
+from src.components import data_transformation
 from src.exception import CustomException
 from src.logger import logging
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -48,3 +52,7 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
